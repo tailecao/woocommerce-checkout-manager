8a350ca8e095a3d493029b16b6e964b17e516330
Tai Le <tai.lecao@email.com>
2025-08-20 10:01:05 +0700

    Initial commit

---
+++ assets/backend/js/admin-field.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('backbone', 'jquery', 'jquery-ui-datepicker', 'wp-util'), 'version' => '0815485febc8e317b67dcc102b05f0c1');
---
+++ templates/order/order-custom-fields.php
@@ -0,0 +1,32 @@
+<?php
+
+$title = get_option( 'wooccm_order_custom_fields_title', esc_html__( 'Order extra', 'woocommerce-checkout-manager' ) );
+
+?>
+
+<h2 class="woocommerce-order-details__title">
+	<?php esc_html_e( $title ); ?>
+</h2>
+<table class="woocommerce-table shop_table order_details">
+	<tbody>
+		<?php
+		if ( count( $checkout = WC()->checkout->get_checkout_fields() ) ) :
+			foreach ( $checkout as $field_type => $fields ) :
+				if ( isset( WOOCCM()->$field_type ) ) :
+					$defaults = array_column( WOOCCM()->$field_type->get_defaults(), 'key' );
+					foreach ( $fields as $key => $field ) :
+						?>
+						<?php if ( ! in_array( $key, $defaults ) && empty( $field['hide_order'] ) ) : ?>
+							<?php if ( $value = get_post_meta( $order_id, sprintf( '_%s', $key ), true ) ) : ?>
+								<tr id="tr-<?php echo esc_attr( $key ); ?>">
+									<th><?php echo esc_html( $field['label'] ); ?></th>
+									<td><?php echo esc_html( $value ); ?></td>
+								</tr>
+							<?php endif; ?>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				<?php endif; ?>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	</tbody>
+</table>
